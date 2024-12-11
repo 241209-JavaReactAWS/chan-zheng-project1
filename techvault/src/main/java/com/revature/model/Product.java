da977d2065cc12dba2475c449d599022c4457109
Mengwai Chan <chanmengwai1@gmail.com>
2024-12-11 18:32:19 -0500

    added model

--- techvault/src/main/java/com/revature/model/Product.java
+++ techvault/src/main/java/com/revature/model/Product.java
@@ -5,36 +5,37 @@ public class Product{
     private String name;
     private double price;
     private String description;
+
+    public void setProductID(int productID){
+        this.productID = productID;
+    }
+
+    public int getProductID(){
+        return this.productID;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public String getDescrption(){
+        return this.description;
+    }
 }
 
-public void setProductID(int productID){
-    this.productID = productID;
-}
-
-public int getProductID(){
-    return this.productID;
-}
-
-public void setName(String name){
-    this.name = name;
-}
-
-public String getName(){
-    return this.name;
-}
-
-public void setPrice(double price){
-    this.price = price;
-}
-
-public double getPrice(){
-    return this.price;
-}
-
-public void setDescription(String description){
-    this.description = description;
-}
-
-public String getDescrption(){
-    return this.description;
-}
--- techvault/src/main/java/com/revature/model/User.java
+++ techvault/src/main/java/com/revature/model/User.java
@@ -4,28 +4,29 @@ public class User {
     private int userID;
     private String username;
     private String password;
-}
 
-public void setUserID(int userID){
-    this.userID = userID;
-}
+    
+    public void setUserID(int userID){
+        this.userID = userID;
+    }
 
-public int getUserID(){
-    return this.userID;
-}
+    public int getUserID(){
+        return this.userID;
+    }
 
-public void setUsername(String username){
-    this.username = username;
-}
+    public void setUsername(String username){
+        this.username = username;
+    }
 
-public String getUsername(){
-    return this.username;
-}
+    public String getUsername(){
+        return this.username;
+    }
 
-public void setPassword(String password){
-    this.password = password;
-}
+    public void setPassword(String password){
+        this.password = password;
+    }
 
-public String getPassword(){
-    return this.password;
+    public String getPassword(){
+        return this.password;
+    }
 }
