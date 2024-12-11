38f2499cc812e925398019352c57f5e8e793d42f
Mengwai Chan <chanmengwai1@gmail.com>
2024-12-11 14:40:33 -0500

    added models

---
+++ techvault/src/main/java/com/revature/model/Product.java
@@ -0,0 +1,40 @@
+package com.revature.models;
+
+public class Product{
+    private int productID;
+    private String name;
+    private double price;
+    private String description;
+}
+
+public void setProductID(int productID){
+    this.productID = productID;
+}
+
+public int getProductID(){
+    return this.productID;
+}
+
+public void setName(String name){
+    this.name = name;
+}
+
+public String getName(){
+    return this.name;
+}
+
+public void setPrice(double price){
+    this.price = price;
+}
+
+public double getPrice(){
+    return this.price;
+}
+
+public void setDescription(String description){
+    this.description = description;
+}
+
+public String getDescrption(){
+    return this.description;
+}
