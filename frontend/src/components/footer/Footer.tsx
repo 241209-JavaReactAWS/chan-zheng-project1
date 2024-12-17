637ef7bca0494e353ab5d54b2b55e375a8c86833
Erin <norozyh@163.com>
2024-12-17 15:44:09 -0500

    Nav,Footer,Logo feature added

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <footer className="footer">
+            &copy; 2024 Techvault. All rights reserved.
+    </footer>
+  )
+}
+
+export default Footer
